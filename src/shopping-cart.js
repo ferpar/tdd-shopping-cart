e2dd529a39ae5df7579027059cc2133d6327506d
fer <ferp@protonmail.ch>
2020-03-15 16:47:33 +0100

    initial commit with jest, and main files

---
+++ src/shopping-cart.js
@@ -0,0 +1,5 @@
+function makeShoppingCart ({ db }) {
+  return Object.freeze({})
+}
+
+module.exports = makeShoppingCart
---
+++ src/shopping-cart.spec.js
@@ -0,0 +1,11 @@
+const makeShoppingCart = require('./shopping-cart.js')
+const makeDb = require('./shopping-cart-db.js')
+
+describe('shopping-cart', () => {
+  it.todo('adds an item')
+  it.todo('removes an item')
+  it.todo("updates an item's quantity")
+  it.todo('lists items')
+  it.todo('calculates a subtotal price')
+
+})
